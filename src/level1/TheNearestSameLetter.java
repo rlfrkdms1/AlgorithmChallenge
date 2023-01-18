8e8f3db145762f5be0410243b6fb71f30929070e
ddukddaki <rlfrkdms1@naver.com>
2023-01-18 23:17:55 +0900

    [Level1] 가장 가까운 같은 글자

---
+++ src/level1/TheNearestSameLetter.java
@@ -0,0 +1,23 @@
+package level1;
+
+public class TheNearestSameLetter {
+
+    /**
+     * @param s 문자열
+     * @return s의 각 위치마다 자신보다 앞에 나왔으면서, 자신과 가장 가까운 곳에 있는 같은 글자가 어디 있는지
+     */
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            answer[i] = -1; //초기화
+            for (int j = 0; j <i; j++) {
+                if (s.substring(j, j + 1).equals(s.substring(i, i + 1))) {
+                    answer[i] = i - j;
+                }
+            }
+        }
+        return answer;
+    }
+
+}
