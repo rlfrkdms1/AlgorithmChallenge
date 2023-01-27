f276592a815ea940f617df1e91a37b8153a98dfe
ddukddaki <rlfrkdms1@naver.com>
2023-01-27 23:45:48 +0900

    [Level1] 삼총사

---
+++ src/level1/Trio.java
@@ -0,0 +1,22 @@
+package level1;
+
+public class Trio {
+    /**
+     * 삼총사
+     * @param number 한국중학교 학생들의 번호를 나타내는 정수 배열
+     * @return 학생들 중 삼총사를 만들 수 있는 방법의 수
+     */
+    public int solution(int[] number) {
+        int answer = 0;
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i+1; j < number.length - 1; j++) {
+                for (int k = j+1; k < number.length - 2; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
