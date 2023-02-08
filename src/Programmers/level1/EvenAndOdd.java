d11fe4a0c70cb40d529a3296cc95bdb60f6f1c49
ddukddaki <rlfrkdms1@naver.com>
2023-02-08 23:57:54 +0900

    [Level1] 짝수와 홀수

---
+++ src/Programmers/level1/EvenAndOdd.java
@@ -0,0 +1,13 @@
+package Programmers.level1;
+
+public class EvenAndOdd {
+    /**
+     * 짝수와 홀수
+     * @param num 정수
+     * @return 짝수면 Even, 홀수면 Odd
+     * 삼항 연산자 연습
+     */
+    public String solution(int num) {
+        return (num % 2 == 0) ? "Even" : "Odd";
+    }
+}
