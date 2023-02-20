69b31b0e5fc6d3c8446a027b37364689877ebf10
ddukddaki <rlfrkdms1@naver.com>
2023-02-20 23:30:49 +0900

    주석 추가

--- src/algorithm/baekjoon/ComplexNumbering.java
+++ src/algorithm/baekjoon/ComplexNumbering.java
@@ -3,6 +3,20 @@ package algorithm.baekjoon;
 import java.io.*;
 import java.util.*;
 
+/**
+ * <문제>
+ *     단지 번호 붙이기
+ *     input : 첫 번째 줄에는 지도의 크기 N(정사각형이므로 가로와 세로의 크기는 같으며 5≤N≤25)이 입력되고, 그 다음 N줄에는 각각 N개의 자료(0혹은 1)가 입력된다.
+ *     output : 첫 번째 줄에는 총 단지수를 출력하시오. 그리고 각 단지내 집의 수를 오름차순으로 정렬하여 한 줄에 하나씩 출력하시오.
+ * </문제>
+ * <풀이>
+ *     dfs를 사용해서 풀이했다.
+ *     groupId를 통해 count를 세었고, 4방 탐색을 이용했다.
+ * </풀이>
+ * <피드백>
+ *     Map 보다 더 좋은 건 없었을까 map은 정렬을 아직 못외워 불안하다. 
+ * </피드백>
+ */
 class ComplexNumbering {
 
     static boolean[][] map;
