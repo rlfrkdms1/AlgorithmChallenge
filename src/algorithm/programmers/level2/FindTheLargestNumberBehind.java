11c332bcce6f55829b3d25d66051780ef2a9c195
rlfrkdms1 <83744709+rlfrkdms1@users.noreply.github.com>
2023-05-21 16:23:39 +0900

    다른 풀이 방법 추가

--- src/algorithm/programmers/level2/FindTheLargestNumberBehind.java
+++ src/algorithm/programmers/level2/FindTheLargestNumberBehind.java
@@ -16,4 +16,31 @@ class FindTheLargestNumberBehind {
         }
         return answer;
     }
+
+    /*
+    그 뒤에 있는 number의 answer에 있는 수를 참고 하는 방법
+    */
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        Arrays.fill(answer, -1);
+        for(int i = numbers.length-1; i >= 0; i--){
+            int target = numbers[i];
+            for(int j = i+1; j < numbers.length; j++){
+                if(target<numbers[j]){
+                    answer[i]=numbers[j];
+                    break;
+                } else {
+                    if(answer[j]==-1){
+                        answer[i] = -1;
+                        break;
+                    }
+                    if(target==numbers[j]){
+                        answer[i]=answer[j];
+                        break;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
 }
