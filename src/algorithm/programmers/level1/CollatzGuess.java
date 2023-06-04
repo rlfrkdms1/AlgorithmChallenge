2868268c6ac5c8dc787c71b8f8f43c2fccbc96e8
rlfrkdms1 <83744709+rlfrkdms1@users.noreply.github.com>
2023-06-04 20:20:26 +0900

    [Level1] 콜라츠 추측

---
+++ src/algorithm/programmers/level1/CollatzGuess.java
@@ -0,0 +1,18 @@
+class CollatzGuess {
+    public int solution(int num) {
+        int answer = 0;
+        long number = (long) num;
+        while(number != 1){
+            if(answer == 500){
+                return -1;
+            }
+            if(number % 2 == 0){
+                number /= 2;
+            } else {
+                number = number * 3 + 1;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
