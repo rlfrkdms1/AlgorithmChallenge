622d15cd54762f823d75a31dbca8461846577e2f
rlfrkdms1 <83744709+rlfrkdms1@users.noreply.github.com>
2023-06-05 23:40:40 +0900

    [Level1] 자릿수 더하기

---
+++ src/algorithm/programmers/level1/AddDigits.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class AddDigits {
+    public int solution(int n) {
+        int answer = 0;
+        String number = Integer.toString(n);
+        for(int i = 0; i < number.length(); i++){
+            answer+=Integer.parseInt(number.substring(i,i+1));
+        }
+
+        return answer;
+    }
+}
